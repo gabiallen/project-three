5022518ac5a8e74b06edb410102e19cc5d21a43f
Gabi Allen <gabijallen@aol.com>
2022-04-25 00:42:02 -0400

    Attempt js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
+const items = document.querySelectorAll(`.item`);
+const detailItem = document.querySelector(`.detail`);
 
+detailScene.style.display = `none`;
+
+items.forEach((item) => {
+  item.addEventListener(`click`, () => {
+    const itemImage = item.querySelector(`img`);
+
+    detailItem
+      .querySelector(`img`)
+      .setAttribute(`src`, itemImage.getAttribute(`src`));
+  });
+});
