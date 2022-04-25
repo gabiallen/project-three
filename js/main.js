b28ff6dd563cee10ec6809b87876348efee6a125
gabiallen <gabiallen@users.noreply.github.com>
2022-04-25 14:44:50 +0000

    deploy: bcf4900e5dbfcf431120bfe8e3780e5b87b7691a

--- js/main.js
+++ js/main.js
@@ -1,14 +1,84 @@
-const items = document.querySelectorAll(`.item`);
-const detailItem = document.querySelector(`.detail`);
+// Get the elements you need to work with
+const items = document.querySelectorAll(".item");
+const detailItem = document.querySelector(".detail");
 
-detailScene.style.display = `none`;
+detailScene.style.display = "none";
 
+// Define the action
 items.forEach((item) => {
-  item.addEventListener(`click`, () => {
-    const itemImage = item.querySelector(`img`);
+  item.addEventListener("click", () => {
+    const itemImage = item.querySelector("img");
 
     detailItem
-      .querySelector(`img`)
-      .setAttribute(`src`, itemImage.getAttribute(`src`));
+      .querySelector("img")
+      .setAttribute("data-image", itemImage.getAttribute("data-key"));
+
+    detailItem
+      .querySelector("img")
+      .setAttribute("src", itemImage.getAttribute("src"));
+
+    let firstRect = itemImage.getBoundingClientRect();
+    let lastRect = detainItem.getBoundingClientRect();
+
+    detailItem.animate(
+      [
+        {
+          transform: `
+          translateX(${firstRect.left - lastRect.left}px)
+          translateY(${firstRect.top - lastRect.top}px)
+          scale(${firstRect.width / lastRect.width})
+          `,
+        },
+        {
+          transform: `
+          translateX(0)
+          translateY(0)
+          scale(1)
+          `,
+        },
+      ],
+      {
+        duration: 600,
+        easing: "cubic-bezier(0,2, 0, 0.2, 1)",
+      }
+    );
   });
+});
+
+// Wire it up
+detailItem.addEventListener("click", () => {
+  const itemImage = document.querySelector(
+    `[data-key="${detailItem.getAttribute("data-image")}"]`
+  );
+
+  let itemImageRect = itemImage.getBoundingClientRect();
+  let detailItemRect = detailItem.getBoundingClientRect();
+
+  detailScene.style.display = "none";
+  itemImage.styleopacity = 1;
+
+  itemImage.animate(
+    [
+      {
+        zIndex: 2,
+        transform: `
+          translateX(${detailItemRect.left - itemImageRect.left}px)
+          translateY(${(detailItemRect.top - itemImageRect, top)}px)
+          scale(${detailItemRect.width / itemImageRect.width})
+          `,
+      },
+      {
+        zIndex: 2,
+        transform: `
+            translateX(0)
+            translateY(0)
+            scale(1)
+            `,
+      },
+    ],
+    {
+      duration: 600,
+      easing: "cubic-bezier(0.2, 0, 0.2, 1)",
+    }
+  );
 });
